2a97ad21b67507648c0f827b7fe284a558676d3e
pozito31 <jessicapozo198715@gmail.com>
2023-03-24 11:30:10 +0100

    subir cambios a github

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { registerLocaleData } from '@angular/common';
+import localEs from '@angular/common/locales/es';
+import localFr from '@angular/common/locales/fr';
+registerLocaleData(localEs);
+registerLocaleData(localFr);
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CapitalizadoPipe } from './pipes/capitalizado.pipe';
+import { ContrasenaPipe } from './pipes/contrasena.pipe';
+import { DomseguroPipe } from './pipes/domseguro.pipe';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [AppComponent, CapitalizadoPipe, ContrasenaPipe, DomseguroPipe],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'es',
+    },
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
